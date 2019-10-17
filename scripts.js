cb0735d6577a777f5322511a98ad6ee3e8258bac
Bill <william.corrie@student.swic.edu>
2019-10-17 18:28:01 -0500

    Add mph calculator function and form reference.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
-// Enter a description of this program here.
+// MPH Calculator will take the distance and time a car travelled to calculate the miles per hour.
 
 /**
  * Write some pseudo code steps here.
  * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
  */
+
+const form = document.querySelector("form");
+const distance = document.querySelector("distance");
+const time = document.querySelector("#time");
+const output = document.querySelector("output");
+
+function mph {return a / b}
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  output.textContent = mph(distance.value, time.value) + " miles per hour";
+});
